40cb83034c52759619e2903fd29d7273f9a9686c
PeraltaHerasAnahiPaulina <al222111387@gmail.com>
2025-02-17 11:56:08 -0600

    Cambios login

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -108,7 +108,7 @@
 <body>
     <header>
         <img src="assets/img/Logo_Pylsa_Colorr.png" alt="Logo de la empresa">
-        <h2>Sistema de Gestión de Líquidos</h2>
+        <h2>SISTEMA DE COTIZACIONES ABAMEX</h2>
     </header>
     <div class="container-wrapper">
         <div class="login-container">
@@ -132,25 +132,14 @@
                 <button type="submit" class="btn btn-primary w-100"><i class="fas fa-sign-in-alt"></i> Iniciar Sesión</button>
             </form>
             <hr>
-            <p class="text-center">o inicia sesión con</p>
-            <div class="d-flex justify-content-center social-login">
-                <a href="#" class="btn btn-primary"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-facebook" viewBox="0 0 16 16">
-                  <path d="M16 8.049c0-4.446-3.582-8.05-8-8.05C3.58 0-.002 3.603-.002 8.05c0 4.017 2.926 7.347 6.75 7.951v-5.625h-2.03V8.05H6.75V6.275c0-2.017 1.195-3.131 3.022-3.131.876 0 1.791.157 1.791.157v1.98h-1.009c-.993 0-1.303.621-1.303 1.258v1.51h2.218l-.354 2.326H9.25V16c3.824-.604 6.75-3.934 6.75-7.951"/>
-                </svg></a>
-                <a href="#" class="btn btn-info"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-twitter-x" viewBox="0 0 16 16">
-                  <path d="M12.6.75h2.454l-5.36 6.142L16 15.25h-4.937l-3.867-5.07-4.425 5.07H.316l5.733-6.57L0 .75h5.063l3.495 4.633L12.601.75Zm-.86 13.028h1.36L4.323 2.145H2.865z"/>
-                </svg></a>
-                <a href="#" class="btn btn-danger"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-google" viewBox="0 0 16 16">
-                  <path d="M15.545 6.558a9.4 9.4 0 0 1 .139 1.626c0 2.434-.87 4.492-2.384 5.885h.002C11.978 15.292 10.158 16 8 16A8 8 0 1 1 8 0a7.7 7.7 0 0 1 5.352 2.082l-2.284 2.284A4.35 4.35 0 0 0 8 3.166c-2.087 0-3.86 1.408-4.492 3.304a4.8 4.8 0 0 0 0 3.063h.003c.635 1.893 2.405 3.301 4.492 3.301 1.078 0 2.004-.276 2.722-.764h-.003a3.7 3.7 0 0 0 1.599-2.431H8v-3.08z"/>
-                </svg></i></a>
-            </div>
+           
         </div>
         <div class="image-container">
-            <img src="assets/img/logo.png" alt="Imagen de inicio">
+            <img src="assets/img/ace.jpg" alt="Imagen de inicio">
         </div>
     </div>
     <footer>
-        <p>&copy; 2025 Sistema de Gestión de Líquidos - Todos los derechos reservados</p>
+        <p>&copy; 2025 Sistema de Cotizaciones ABAMEX - Todos los derechos reservados</p>
     </footer>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
 </body>
