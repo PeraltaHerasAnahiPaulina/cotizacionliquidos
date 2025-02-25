6a82576688f5c84113184df2dd3d7e9f8a699252
Alma Delia Duro Heras <almadeliaheras691@gmail.com>
2025-02-25 09:12:55 -0600

    cambios controller

---
+++ resources/views/gerenciaventas/catalogo/client.blade.php
@@ -0,0 +1,212 @@
+@extends('gerenciaventas.layouts.templatep')
+
+
+@section('contenido')
+
+
+<div class="page-header pb-10 page-header-dark bg-gradient-primary-to-secondary">
+    <div class="container-fluid">
+        <div class="page-header-content">
+            <h1 class="page-header-title">
+                <div class="page-header-icon"><i data-feather="file"></i></div>
+                <span>CLIENTES</span>
+            </h1>
+            <div class="page-header-subtitle">Reporte de clientes</div>
+        </div>
+    </div>
+</div>
+<div class="container-fluid mt-n10">
+    <div class="card">
+        <div class="card-body">
+
+
+
+
+            <div class="panel panel-default">
+                <div class="panel-body">
+                    <div id="list" class="table-responsive">
+                           
+                        <!-- ↓Id de la tabla (Es importante colocar un id) ↓ -->
+                        <!--↓Quitar cuando se acomode dentro de un div bueno↓-->
+                        <table id="TablaEXL" class="table table-striped table-bordered display" style="width: 100%">
+                            <thead>
+                                <tr style="background-color: #E0E0E0">
+                                    <th style="background-color: #323F52; color: #ffffff">Clave Sap</th>
+                                    <th style="background-color: #323F52; color: #ffffff">Razon Social</th>
+                                    <th style="background-color: #323F52; color: #ffffff">Contacto</th>
+                                    <th style="background-color: #323F52; color: #ffffff">Direccion</th>
+                                    <th style="background-color: #323F52; color: #ffffff; width:25px">Acciones</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr><!--↓ Es recomendado usar campos de tipo Text para las fechas en ese caso colocar este codigo!! ↓-->
+                                    <td>123434</td>
+                                    <td>Persona Fisica</td>
+                                    <td>722254522</td>
+                                    <td>Calle Aventura #10</td>
+                                    <td><button class="btn btn-datatable btn-icon btn-transparent-dark mr-2" 
+                                        onclick="window.location='{{ route('edicion',11) }}'">
+                                        <i class="fa-solid fa-pencil" style="color: #FFD43B;"></i>
+                                    </button>
+                                        <button class="btn btn-datatable btn-icon btn-transparent-dark" data-toggle="modal" data-target="#desactivarModal" onclick="confirmDesactivation()">
+                                        <i class="fa-solid fa-ban" style="color: #ff0000;"></i>
+                                    </button>
+                                </td>
+                                          
+
+                                </tr>
+                                <tr>
+                                    <td>456789</td>
+                                    <td>Persona Fisica</td>
+                                    <td>722222454</td>
+                                    <td>Calle Hidalgo #702</td>
+                                    <td><button class="btn btn-datatable btn-icon btn-transparent-dark mr-2" 
+                                        onclick="window.location='{{ route('edicion',11) }}'">
+                                        <i class="fa-solid fa-pencil" style="color: #FFD43B;"></i>
+                                    </button><button class="btn btn-datatable btn-icon btn-transparent-dark" data-toggle="modal" data-target="#desactivarModal" onclick="confirmDesactivation()">
+                                            <i class="fa-solid fa-ban" style="color: #ff0000;"></i>
+                                        </button>
+                                <tr>
+                                    <td>245675</td>
+                                    <td>Persona Fisica</td>
+                                    <td>556475211</td>
+                                    <td>Calle libre #099</td>
+                                    <td><button class="btn btn-datatable btn-icon btn-transparent-dark mr-2" 
+                                        onclick="window.location='{{ route('edicion',11) }}'">
+                                        <i class="fa-solid fa-pencil" style="color: #FFD43B;"></i>
+                                    </button><button class="btn btn-datatable btn-icon btn-transparent-dark" data-toggle="modal" data-target="#desactivarModal" onclick="confirmDesactivation()">
+                                            <i class="fa-solid fa-ban" style="color: #ff0000;"></i>
+                                        </button>
+                                        
+                                </tr>
+                                <tr>
+                                    <td>245678</td>
+                                    <td>Persona Fisica</td>
+                                    <td>7894523451</td>
+                                    <td>Calle Soledad #304</td>
+                                    <td><button class="btn btn-datatable btn-icon btn-transparent-dark mr-2" 
+                                        onclick="window.location='{{ route('edicion',11) }}'">
+                                        <i class="fa-solid fa-pencil" style="color: #FFD43B;"></i>
+                                    </button><button class="btn btn-datatable btn-icon btn-transparent-dark" data-toggle="modal" data-target="#desactivarModal" onclick="confirmDesactivation()">
+                                            <i class="fa-solid fa-ban" style="color: #ff0000;"></i>
+                                        </button>
+                                </tr>
+                                <tr>
+                                    <td>245678</td>
+                                    <td>Persona Fisica </td>
+                                    <td>775566999</td>
+                                    <td>Calle San Lorenzo #123</td>
+                                    <td><button class="btn btn-datatable btn-icon btn-transparent-dark mr-2" 
+                                        onclick="window.location='{{ route('edicion',11) }}'">
+                                        <i class="fa-solid fa-pencil" style="color: #FFD43B;"></i>
+                                    </button><button class="btn btn-datatable btn-icon btn-transparent-dark" data-toggle="modal" data-target="#desactivarModal" onclick="confirmDesactivation()">
+                                            <i class="fa-solid fa-ban" style="color: #ff0000;"></i>
+                                        </button>
+                                </tr>
+                                <tr>
+                                    <td>212678</td>
+                                    <td>Persona Fisica</td>
+                                    <td>758468255</td>
+                                    <td>Calle La Esperanza #108</td>
+                                    <td><button class="btn btn-datatable btn-icon btn-transparent-dark mr-2" 
+                                        onclick="window.location='{{ route('edicion',11) }}'">
+                                        <i class="fa-solid fa-pencil" style="color: #FFD43B;"></i>
+                                    </button><button class="btn btn-datatable btn-icon btn-transparent-dark" data-toggle="modal" data-target="#desactivarModal" onclick="confirmDesactivation()">
+                                            <i class="fa-solid fa-ban" style="color: #ff0000;"></i>
+                                        </button>
+                                </tr>
+                                <tr>
+                                    <td>233378</td>
+                                    <td>Persona Fisica</td>
+                                    <td>7894663112</td>
+                                    <td>Calle Potrero #200</td>
+                                    <td><button class="btn btn-datatable btn-icon btn-transparent-dark mr-2" 
+                                        onclick="window.location='{{ route('edicion',11) }}'">
+                                        <i class="fa-solid fa-pencil" style="color: #FFD43B;"></i>
+                                    </button><button class="btn btn-datatable btn-icon btn-transparent-dark" data-toggle="modal" data-target="#desactivarModal" onclick="confirmDesactivation()">
+                                            <i class="fa-solid fa-ban" style="color: #ff0000;"></i>
+                                        </button>
+                                </tr>
+                                <tr>
+                                    <td>773378</td>
+                                    <td>Persona Fisica </td>
+                                    <td>722236914</td>
+                                    <td>Calle Altamirano #504</td>
+                                    <td><button class="btn btn-datatable btn-icon btn-transparent-dark mr-2" 
+                                        onclick="window.location='{{ route('edicion',11) }}'">
+                                        <i class="fa-solid fa-pencil" style="color: #FFD43B;"></i>
+                                    </button><button class="btn btn-datatable btn-icon btn-transparent-dark" data-toggle="modal" data-target="#desactivarModal" onclick="confirmDesactivation()">
+                                            <i class="fa-solid fa-ban" style="color: #ff0000;"></i>
+                                        </button>
+                                </tr>
+                                <tr>
+                                    <td>888378</td>
+                                    <td>Persona Fisica </td>
+                                    <td>775828512</td>
+                                    <td>Calle del Carmen</td>
+                                    <td><button class="btn btn-datatable btn-icon btn-transparent-dark mr-2" 
+                                        onclick="window.location='{{ route('edicion',11) }}'">
+                                        <i class="fa-solid fa-pencil" style="color: #FFD43B;"></i>
+                                    </button><button class="btn btn-datatable btn-icon btn-transparent-dark" data-toggle="modal" data-target="#desactivarModal" onclick="confirmDesactivation()">
+                                            <i class="fa-solid fa-ban" style="color: #ff0000;"></i>
+                                        </button>
+                                </tr>
+                                <tr>
+                                    <td>111378</td>
+                                    <td>Persona Fisica</td>
+                                    <td>729645882</td>
+                                    <td>Calle Madero #222</td>
+                                    <td><button class="btn btn-datatable btn-icon btn-transparent-dark mr-2" 
+                                        onclick="window.location='{{ route('edicion',11) }}'">
+                                        <i class="fa-solid fa-pencil" style="color: #FFD43B;"></i>
+                                    </button><button class="btn btn-datatable btn-icon btn-transparent-dark" data-toggle="modal" data-target="#desactivarModal" onclick="confirmDesactivation()">
+                                            <i class="fa-solid fa-ban" style="color: #ff0000;"></i>
+                                        </button>
+                                </tr>
+                                <tr>
+                                    <td>111378</td>
+                                    <td>Persona Fisica </td>
+                                    <td>7285645681</td>
+                                    <td>Calle 16 de Septiembre </td>
+                                    <td><button class="btn btn-datatable btn-icon btn-transparent-dark mr-2" 
+                                        onclick="window.location='{{ route('edicion',11) }}'">
+                                        <i class="fa-solid fa-pencil" style="color: #FFD43B;"></i>
+                                    </button><button class="btn btn-datatable btn-icon btn-transparent-dark" data-toggle="modal" data-target="#desactivarModal" onclick="confirmDesactivation()">
+                                            <i class="fa-solid fa-ban" style="color: #ff0000;"></i>
+                                        </button>
+                                </tr>
+
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </div>
+<script>
+    function confirmDesactivation() {
+        Swal.fire({
+            title: 'Desactivar Usuario',
+            text: 'Al desactivar el usuario este dejará de tener acceso al sistema.',
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonText: 'Desactivar',
+            cancelButtonText: 'Cancelar',
+            reverseButtons: true
+        }).then((result) => {
+            if (result.isConfirmed) {
+                // Aquí puedes agregar la lógica para desactivar al cliente.
+                Swal.fire(
+                    'Desactivado!',
+                    'El usuario ha sido desactivado.',
+                    'success'
+                );
+            }
+        });
+    }
+</script>
+
+
+
+    @endsection
---
+++ resources/views/gerenciaventas/catalogo/edicion.blade.php
@@ -0,0 +1,146 @@
+@extends('gerenciaventas.layouts.templatep')
+
+
+@section('contenido')
+
+
+    <div class="page-header pb-10 page-header-dark bg-gradient-primary-to-secondary">
+        <div class="container-fluid">
+            <div class="page-header-content">
+                <h1 class="page-header-title">
+                    <div class="page-header-icon"><i data-feather="file"></i></div>
+                    <span>REPORTE</span>
+                </h1>
+                <div class="page-header-subtitle">Informacion Cliente</div>
+            </div>
+        </div>
+    </div>
+    <div class="container-fluid mt-n10">
+        <div class="card">
+            <div class="card-body">
+                <div class="container mt-5">
+                    <ul class="nav nav-tabs" id="formTabs" role="tablist">
+                        <li class="nav-item">
+                            <a class="nav-link active" id="informacion-sap-tab" data-toggle="tab" href="#informacion-sap"
+                                role="tab" aria-controls="informacion-sap" aria-selected="true">Información SAP</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" id="informacion-tab" data-toggle="tab" href="#informacion" role="tab"
+                                aria-controls="informacion" aria-selected="false">Informacion Sistema</a>
+                        </li>
+                    </ul>
+                    <div class="tab-content mt-4" id="formTabsContent">
+                        <div class="tab-pane fade show active" id="informacion-sap" role="tabpanel"
+                            aria-labelledby="informacion-sap-tab">
+                            <form class="needs-validation" novalidate>
+                                <div class="border p-4 rounded">
+                                    <h4 class="mb-3">Información SAP</h4>
+                                    <div class="form-row mb-3">
+                                        <div class="col-md-6">
+                                            <label for="clave" class="form-label">* Clave:</label>
+                                            <input type="text" class="form-control" id="clave" placeholder="861111" required>
+                                            <div class="invalid-feedback">Por favor, ingresa la clave.</div>
+                                        </div>
+                                        <div class="col-md-6">
+                                            <label for="clave" class="form-label">* Razon social:</label>
+                                            <input type="text" class="form-control" id="clave" placeholder="Persona Moral" required>
+                                            <div class="invalid-feedback">Por favor, ingresa Razon social.</div>
+                                        </div>
+                                        <div class="col-md-6">
+                                            <label for="no-producto" class="form-label">*Contacto:</label>
+                                            <input type="text" class="form-control" id="razon-social" placeholder="78979846" required>
+                                            <div class="invalid-feedback">Por favor, ingresa Contacto.</div>
+                                        </div>
+                                    </div>
+                                    <div class="form-row mb-3">
+                                        <div class="col-md-6">
+                                            <label for="nombre" class="form-label">* Direccion:</label>
+                                            <input type="text" class="form-control" id="contacto" placeholder="Calle Hidalgo" required>
+                                            <div class="invalid-feedback">Por favor, ingresa direccion.</div>
+                                        </div>
+                                        
+                                    </div>
+                                    <div class="form-row justify-content-center mt-4">
+                                        <button type="submit" class="btn btn-primary mx-2" onclick="window.location='{{ route('client') }}'">Guardar</button>
+                                        <button type="reset" class="btn btn-secondary mx-2" onclick="window.location='{{ route('client') }}'">Cancelar</button>
+                                     </div>
+                                </div>
+                            </form>
+                        </div>
+                        
+                        <div class="tab-pane fade" id="informacion" role="tabpanel" aria-labelledby="informacion-tab">
+                            <form class="needs-validation" novalidate>
+                                <div class="border p-4 rounded">
+                                    <h4 class="mb-3">Información Sistema</h4>
+                                    <div class="form-row mb-3">
+                                        <div class="col-md-6">
+                                            <label for="atencion" class="form-label">* Atencion:</label>
+                                            <input type="text" class="form-control" id="atencion" placeholder="" required>
+                                            <div class="invalid-feedback">Por favor, ingresa atencion.</div>
+                                        </div>
+                                        <div class="col-md-6">
+                                            <label for="email" class="form-label">* Email:</label>
+                                            <input type="text" class="form-control" id="email" placeholder="juan@gmail.com" required>
+                                            <div class="invalid-feedback">Por favor, ingresa email.</div>
+                                        </div>
+                                        <div class="col-md-6">
+                                            <label for="telefono" class="form-label">*Telefono:</label>
+                                            <input type="text" class="form-control" id="telefono" placeholder="75648413654" required>
+                                            <div class="invalid-feedback">Por favor, ingresa un telefono.</div>
+                                        </div>
+                                    </div>
+                                    <div class="col-md-6">
+                                            <label class="form-label">* Moneda:</label>
+                                            <div>
+                                                <div class="form-check form-check-inline">
+                                                    <input class="form-check-input" type="radio" name="moneda"
+                                                        id="activo-moneda" value="moneda" checked id="activo-moneda" required>
+                                                    <label class="form-check-label" for="activo-moneda" >MXN </label>
+                                                </div>
+                                                <div class="form-check form-check-inline">
+                                                    <input class="form-check-input" type="radio" name="moneda"
+                                                        id="activo-moneda" value="moneda" required>
+                                                    <label class="form-check-label" for="activo-moneda">USD</label>
+                                                </div>
+                                                <div class="col-md-6">
+                                            <label class="form-label">* Forma de Pago:</label>
+                                            <div>
+                                                <div class="form-check form-check-inline">
+                                                    <input class="form-check-input" type="radio" name="pago"
+                                                        id="activo-pago" value="pago" required>
+                                                    <label class="form-check-label" for="activo-credito">Credito </label>
+                                                </div>
+                                                <div class="form-check form-check-inline">
+                                                    <input class="form-check-input" type="radio" name="pago"
+                                                        id="activo-pago" value="pago" checked id="activo-pago" required>
+                                                    <label class="form-check-label" for="activo-contado">Contado</label>
+                                                </div>
+                                </div>
+        
+                            </form>
+                        </div>
+                        
+                    <div class="form-row justify-content-center mt-4">
+                        <button type="submit" class="btn btn-primary mx-2" onclick="window.location='{{ route('client') }}'">Guardar</button>
+                        <button type="reset" class="btn btn-secondary mx-2" onclick="window.location='{{ route('client') }}'">Cancelar</button>
+                    </div>
+                </div>
+
+                <script>
+                    function previewImage(event) {
+                        var reader = new FileReader();
+                        reader.onload = function () {
+                            var output = document.getElementById('preview');
+                            output.src = reader.result;
+                            output.classList.remove('d-none');
+                        };
+                        reader.readAsDataURL(event.target.files[0]);
+                    }
+                </script>
+
+            </div>
+        </div>
+
+
+
+@endsection
